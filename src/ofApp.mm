f0e6c493df69683ddc97809ee4c47cddff10a331
yutatoga <yuta.toga@gmail.com>
2014-05-12 04:10:16 +0900

    update for iPad

--- src/ofApp.mm
+++ src/ofApp.mm
@@ -22,28 +22,66 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofSetColor(255);
-    grabber.draw(0, 0, ofGetWidth(), ofGetHeight());
-    ofRect(0, 0, 100, 50);
-    originImage.draw(ofPoint(0, 0), ofGetWidth(), ofGetHeight());
-    ofColor originColor, canvasColor, grabberColor;
-    ofImage grabberImage;
-    grabber.draw(0, 0, ofGetWidth(), ofGetHeight());
-    grabberImage.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
-    for (int i=0; i<ofGetHeight(); i++) {
-        for (int j=0; j<ofGetWidth(); j++) {
-            //カメラからの入力した色が、今のカンバスの色よりも近い場合は、塗り替える
-            grabberColor = grabberImage.getColor(j, i);
-            canvasColor = canvasImage.getColor(j, i);
-            originColor = originImage.getColor(j, i);
-            if (colorDistance(grabberColor, originColor) < colorDistance(canvasColor, originColor)) {
-                canvasImage.setColor(j, i, grabberColor);
-            }
-            
-        }
-    }
-    canvasImage.update();
-    canvasImage.draw(ofPoint(0,0), ofGetWidth(), ofGetHeight());
+		if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad){
+				// The device is an iPad running iOS 3.2 or later.
+				// The device is an iPhone or iPod touch.
+				ofSetColor(255);
+				grabber.draw(0, 0, ofGetWidth(), ofGetHeight());
+				ofRect(0, 0, 100, 50);
+				originImage.draw(ofPoint(0, 0), ofGetWidth(), ofGetHeight());
+				ofColor originColor, canvasColor, grabberColor;
+				ofImage grabberImage;
+				grabber.draw(0, 0, ofGetWidth(), ofGetHeight());
+				grabberImage.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
+				grabberImage.resize(ofGetWidth()/2, ofGetHeight()/2);
+				ofLog(OF_LOG_NOTICE, "canvas");
+				ofLog(OF_LOG_NOTICE, "w"+ofToString(canvasImage.width)+"="+ofToString(ofGetWidth()));
+				ofLog(OF_LOG_NOTICE, "h"+ofToString(canvasImage.height)+"="+ofToString(ofGetHeight()));
+				ofLog(OF_LOG_NOTICE, "monalisa");
+				ofLog(OF_LOG_NOTICE, "w"+ofToString(originImage.width)+"="+ofToString(ofGetWidth()));
+				ofLog(OF_LOG_NOTICE, "h"+ofToString(originImage.height)+"="+ofToString(ofGetHeight()));
+				ofLog(OF_LOG_NOTICE, "grabberImage");
+				ofLog(OF_LOG_NOTICE, "w"+ofToString(grabberImage.width)+"="+ofToString(ofGetWidth()));
+				ofLog(OF_LOG_NOTICE, "h"+ofToString(grabberImage.height)+"="+ofToString(ofGetHeight()));
+				
+				for (int i=0; i<ofGetHeight()/2; i++) {
+						for (int j=0; j<ofGetWidth()/2; j++) {
+								//カメラからの入力した色が、今のカンバスの色よりも近い場合は、塗り替える
+								grabberColor = grabberImage.getColor(j, i);
+								canvasColor = canvasImage.getColor(j, i);
+								originColor = originImage.getColor(j, i);
+								if (colorDistance(grabberColor, originColor) < colorDistance(canvasColor, originColor)) {
+										canvasImage.setColor(j, i, grabberColor);
+								}
+						}
+				}
+				canvasImage.update();
+				canvasImage.draw(ofPoint(0,0), ofGetWidth(), ofGetHeight());
+		}else{
+				// The device is an iPhone or iPod touch.
+				ofSetColor(255);
+				grabber.draw(0, 0, ofGetWidth(), ofGetHeight());
+				ofRect(0, 0, 100, 50);
+				originImage.draw(ofPoint(0, 0), ofGetWidth(), ofGetHeight());
+				ofColor originColor, canvasColor, grabberColor;
+				ofImage grabberImage;
+				grabber.draw(0, 0, ofGetWidth(), ofGetHeight());
+				grabberImage.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
+				for (int i=0; i<ofGetHeight(); i++) {
+						for (int j=0; j<ofGetWidth(); j++) {
+								//カメラからの入力した色が、今のカンバスの色よりも近い場合は、塗り替える
+								grabberColor = grabberImage.getColor(j, i);
+								canvasColor = canvasImage.getColor(j, i);
+								originColor = originImage.getColor(j, i);
+								if (colorDistance(grabberColor, originColor) < colorDistance(canvasColor, originColor)) {
+										canvasImage.setColor(j, i, grabberColor);
+								}
+								
+						}
+				}
+				canvasImage.update();
+				canvasImage.draw(ofPoint(0,0), ofGetWidth(), ofGetHeight());
+		}
 }
 
 float ofApp::colorDistance(ofColor color1, ofColor color2){
